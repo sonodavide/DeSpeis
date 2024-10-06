693197cf72c394e47d2d13434d6eeba93d378bf5
DividingByZeros <davide.pupino1@gmail.com>
2024-10-07 01:43:46 +0200

    dto stuff

---
+++ src/main/java/org/example/despeis/dto/PostiSpettacoloResponseDto.java
@@ -0,0 +1,20 @@
+package org.example.despeis.dto;
+
+import lombok.Value;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+@Value
+public class PostiSpettacoloResponseDto implements Serializable {
+    Integer spettacoloId;
+    Map<String, List<PostoResponse>> posti;
+
+    @Value
+    public static class PostoResponse{
+        Integer postoId;
+        Integer postoSedileId;
+        String stato;
+    }
+}
